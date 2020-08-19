7387fe295a9a5375827f9ffdfeb32ce3ebded919
Mattias Nyberg <muthaias.uu@gmail.com>
2020-08-19 13:00:10 +0200

    wip: add generic command line description in example file

--- example_run.sh
+++ example_run.sh
@@ -1,1 +1,5 @@
+# Generisk beskrivning:
+#  * num_procs: Antalet process som process-poolen får använda under körning
+#  * file_glob_x: En filsökväg som får innehålla wildcards (*) för att matcha filer
+# python parallel_search.py <num_procs> <file_glob_0> ... <file_glob_N>
 python parallel_search.py 2 data/*.txt
